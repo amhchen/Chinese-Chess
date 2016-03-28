517775634b6b7f20eaa7d39d3f3139ee59dee9ec
amhchen <abelchen923@gmail.com>
2016-03-28 11:40:35 -0400

    Create horse.java

---
+++ Model/horse.java
@@ -0,0 +1,14 @@
+//example horse class
+
+public class Horse extends Piece {
+  //constructor
+  @override
+  void make_move(int x, int y) {
+   //code
+   }
+   
+   @override
+   bool make_move(int x, int y) {
+     //code 
+  }
+}
