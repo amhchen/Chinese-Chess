7d685cc425befaacbed654f4d1a0f8acb354e55e
amhchen <abelchen923@gmail.com>
2016-03-29 19:07:07 -0400

    Update Game.java

--- Model/Game.java
+++ Model/Game.java
@@ -1,3 +1,7 @@
+// dunno how you want to implement this, do whatever you think is best.
+// should probably have a function is_occupied() since the array of pieces is stored here
+// get each piece to call this function in their valid_move() when necessary
+
 package gameapps.chinesechess;
 import java.util.Vector;
 
