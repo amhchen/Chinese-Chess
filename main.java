2df5b761931446adbb1f6a2571755dfb23f04787
amhchen <abelchen923@gmail.com>
2016-03-29 11:13:30 -0400

    Added files via upload

--- main.java
+++ main.java
@@ -1,13 +1,33 @@
 package gameapps.chinesechess;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class main extends AppCompatActivity {
+public class main extends AppCompatActivity implements View.OnClickListener{
+    private Button btnPlay;
+    private TextView tvresult;
+    private EditText num1,num2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate (Bundle SavedInstanceState)  {
+        super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        btnPlay = (Button)findViewById(R.id.playbtn);
+        btnPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Board.class);
+        startActivity(intent);
     }
 }
