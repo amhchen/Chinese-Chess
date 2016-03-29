d0b6849a5d8ae582f94a6ef74366ee6b5acbf966
amhchen <abelchen923@gmail.com>
2016-03-29 19:06:45 -0400

    Delete game.java

--- Model/game.java
+++
@@ -1,3 +0,0 @@
-// dunno how you want to implement this, do whatever you think is best.
-// should probably have a function is_occupied() since the array of pieces is stored here
-// get each piece to call this function in their valid_move() when necessary
