7b47442bf33b9642e2edfe5f8245f553ad85cb15
amhchen <abelchen923@gmail.com>
2016-04-08 17:38:53 -0400

    Create Elephant.java
    Completed Elephant's ctor and valid_move()

---
+++ Model/Elephant.java
@@ -0,0 +1,38 @@
+public class Elephant extends Piece {
+  public Elephant(int x_init, int y_init, String col_set) {
+    x = x_init;
+    y = y_init;
+    colour = col_set;
+  }
+  @override
+  bool valid_move(int x_dest, int y_dest) {
+    int y_min = 0;
+    int y_max = 4;
+    if (colour == "black") {
+      y_min = 5;
+      y_max = 9;
+    }
+    if (0 <= x_dest <= 8 && y_min <= y_dest <= y_max) {
+     if (is_occupied(x_dest,y_dest) != colour) {
+       int x_length = x - x_dest;
+       int y_length = y - y_dest;
+       if (abs(x_length) == 2 &&abs(y_length) == 2) {
+         if (is_occupied(x + (x_length/2), y + (y_length/2)) == "free") {
+           return true;
+         }
+       }
+      }
+    }
+    return false;
+  }
+   
+   @override
+   bool make_move(int x_dest, int y_dest) {
+     if (valid_move(x_dest,y_dest)) {
+       x = x_dest;
+       y = y_dest;
+       return true;
+     }
+     return false;
+  }
+}
